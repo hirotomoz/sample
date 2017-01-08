546f07d5533b7a98cc5e50bb177181b055ba2a0b
Tomohiro Zama <tzama001@yahoo.co.jp>
2017-01-08 13:43:34 +0900

    add test5

--- SampleTest.php
+++ SampleTest.php
@@ -13,6 +13,7 @@ class SampleTest extends PHPUnit_Framework_TestCase
      */
     public function aaa()
     {
+        echo "aaa start";
         $Sample = new Sample();
         $this->assertEquals($Sample->helloWorld(), 'hello world!');
     }
@@ -21,6 +22,7 @@ class SampleTest extends PHPUnit_Framework_TestCase
      */
     public function bbb()
     {
+        echo "bbb start";
         $Sample = new Sample();
         $this->assertEquals($Sample->helloWorld(), 'hello world!');
     }
